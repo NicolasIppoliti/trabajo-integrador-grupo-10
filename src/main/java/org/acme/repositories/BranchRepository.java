73934fe3ee9f3a9717ec3aa682826a64076cd406
MatiCampo96 <maticampopiano@hotmail.com>
2024-06-05 20:35:15 -0300

    arreglo pequeño error en branchRepository

--- src/main/java/org/acme/repositories/BranchRepository.java
+++ src/main/java/org/acme/repositories/BranchRepository.java
@@ -1,10 +1,11 @@
 package org.acme.repositories;
 
 import org.acme.entities.Branch;
+
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class BranchRepository implements PanacheRepository<Branch> {
-	
+public class BranchRepository implements PanacheRepository<Branch>{
+
 }
