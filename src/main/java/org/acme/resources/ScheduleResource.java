10c82622fca2c4927aff82927a2c5eb1b6af9d5f
MatiCampo96 <maticampopiano@hotmail.com>
2024-06-05 21:11:04 -0300

    Agrego recursos basicos de horarios y doctors

--- src/main/java/org/acme/resources/ScheduleResource.java
+++ src/main/java/org/acme/resources/ScheduleResource.java
@@ -1,5 +1,58 @@
 package org.acme.resources;
 
+import java.util.List;
+
+import org.acme.entities.Schedule;
+import org.acme.repositories.ScheduleRepository;
+import org.acme.repositories.ScheduleRepository;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.WebApplicationException;
+
 public class ScheduleResource {
 
+	@Inject
+	ScheduleRepository scheduleRepository;
+	
+    @GET
+    public List<Schedule> getSchedules() {
+        return scheduleRepository.listAll();
+    }
+
+    @POST
+    public Schedule createSchedule(Schedule schedule) {
+    	scheduleRepository.persist(schedule);
+        return schedule;
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Schedule updateSchedule(@PathParam("id") Long id, Schedule schedule) {
+    	Schedule entity = scheduleRepository.findById(id);
+        if (entity == null) {
+            throw new WebApplicationException("Horario no encontrado", 404);
+        }
+        entity.setDay(schedule.getDay());
+        entity.setEntry_time(schedule.getEntry_time());
+        entity.setDeparture_time(schedule.getDeparture_time());
+        scheduleRepository.persist(entity);
+        return entity;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void deleteSchedule(@PathParam("id") Long id) {
+    	Schedule entity = scheduleRepository.findById(id);
+        if (entity == null) {
+            throw new WebApplicationException("Horario no encontrado", 404);
+        }
+        scheduleRepository.delete(entity);
+    }
 }
+
